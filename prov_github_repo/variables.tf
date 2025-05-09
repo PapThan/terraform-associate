e54cbfd3ac3226beb3c6e8c72d1c093571d32b90
thanosPapach <thanospapacharalampous@outlook.com>
2025-05-09 09:49:23 +0000

    correct token

--- prov_github_repo/variables.tf
+++ prov_github_repo/variables.tf
@@ -1,5 +1,5 @@
 variable "github_token" {
   description = "GitHub token for authentication"
   type        = string
-  default     = "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"
+  default     = "github_pat"
 }
